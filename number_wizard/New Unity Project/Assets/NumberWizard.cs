8483668113f67dc439e527a34b25c00508e8d6c2
Angela Gloyna <angela.gloyna@gmail.com>
2020-08-22 11:31:53 -0500

    finish up intro text game

--- number_wizard/New Unity Project/Assets/NumberWizard.cs
+++ number_wizard/New Unity Project/Assets/NumberWizard.cs
@@ -4,20 +4,57 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int max;
+    int min;
+    int guess;
+
     // Start is called before the first frame update
     void Start()
     {
-        int max = 1000;
-        int min = 1;
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
         Debug.Log("Welcome to Number Wizard");
         Debug.Log("Pick a number");
         Debug.Log("The highest number is: " + max);
         Debug.Log("The lowest number is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than my guess: " + guess);
+        Debug.Log("Push Up if higher, Push Down if lower, Push Enter if Correct");
+        max = max + 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow Key was pressed.");
+            min = guess;
+            RerollGuess();
+            Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow Key was pressed.");
+            max = guess;
+            RerollGuess();
+            Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter was pressed.");
+            Debug.Log("Yay!!!");
+            StartGame();
+        }
+    }
+    void RerollGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is your number higher or lower than " + guess);
     }
 }
